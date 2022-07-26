044b927908f2b7d841ccc16d00a34ec370e14415
Sarah S <smarahq@gmail.com>
2022-07-26 18:43:15 +1000

    setup chatbot component

---
+++ chatbot/src/App.js
@@ -0,0 +1,20 @@
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+import config from './bot/config.jsx';
+import MessageParser from './bot/MessageParser.jsx';
+import ActionProvider from './bot/ActionProvider.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+    </div>
+  );
+}
+
+export default App;
