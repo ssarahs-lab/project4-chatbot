1dad276afc0c4d1a41b8230fe560af0cb13b6127
Sarah S <smarahq@gmail.com>
2022-07-31 12:33:10 +1000

    added route paths

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -4,13 +4,43 @@ import config from './bot/config.jsx';
 import MessageParser from './bot/MessageParser.jsx';
 import ActionProvider from './bot/ActionProvider.jsx';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+  <div className="App">
+  
+      <header>
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/chatpage">Chat with Saura</a></li>
+          <li> <a href="/animalgenerate">Generate an Animal - test example</a></li>
+        </ul>
+      </header>
+     
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/chatpage" element={<Chatpage/>}/>
+        <Route path="/animalgenerate" element={<Generate/>}/>
+      </Routes>
+    </BrowserRouter>
+  
+  </div>
+     
+  );
+}
 
-    
-     <Chatbot
+function Home() {
+  return ( <div>
+    <h1>Home</h1>
+    <p>Welcome!</p>
+  </div>);
+}
+
+function Chatpage() {
+  return (  <div>
+    <Chatbot
         config={config}
         messageParser={MessageParser}
         actionProvider={ActionProvider}
@@ -18,5 +48,12 @@ function App() {
     </div>
   );
 }
+
+function Generate() {
+  return ( <div></div> );
+}
+
+
+
 
 export default App;
